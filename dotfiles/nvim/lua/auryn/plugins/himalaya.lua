207ff96fcf13b981bb39d3c90fb3e92acb94891e
Auryn <auryn31@gmx.de>
2026-01-26 18:22:23 +0100

    remove himalaya

--- dotfiles/nvim/lua/auryn/plugins/himalaya.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"pimalaya/himalaya-vim",
-	branch = "master",
-	config = function()
-		vim.g.himalaya_executable = "/opt/homebrew/bin/himalaya"
-		vim.g.himalaya_folder_picker = "telescope"
-		vim.g.himalaya_folder_picker_telescope_preview = false
-	end,
-}
