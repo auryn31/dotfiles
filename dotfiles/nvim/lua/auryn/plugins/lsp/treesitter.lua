4fccc9383834e33da73c280696a3f31bd92be1c8
Auryn <auryn31@gmx.de>
2026-02-06 15:44:53 +0100

    move plugins

--- dotfiles/nvim/lua/auryn/plugins/lsp/treesitter.lua
+++ dotfiles/nvim/lua/auryn/plugins/lsp/treesitter.lua
@@ -1,21 +1,31 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	version = false, -- last release is way too old and doesn't work on Windows
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	dependencies = {
-		{
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-	},
-	cmd = { "TSUpdateSync" },
-	keys = {
-		{ "<c-space>", desc = "Increment selection" },
-		{ "<bs>", desc = "Decrement selection", mode = "x" },
-	},
+	lazy = false,
+	-- build = ":TSUpdate",
+	-- event = { "BufReadPost", "BufNewFile" },
+	-- dependencies = {
+	-- 	"nvim-treesitter/nvim-treesitter-textobjects",
+	-- },
+	-- cmd = { "TSUpdateSync" },
+	-- keys = {
+	-- 	{ "<c-space>", desc = "Increment selection" },
+	-- 	{ "<bs>", desc = "Decrement selection", mode = "x" },
+	-- },
 	opts = {
+		auto_install = true,
 		highlight = { enable = true },
-		indent = { enable = true },
+		indent = {
+			enable = true,
+		},
+		autopairs = {
+			enable = true,
+		},
+		autotag = {
+			enable = false,
+		},
+		endwise = {
+			enable = true,
+		},
 		ensure_installed = {
 			"bash",
 			"c",
@@ -42,28 +52,14 @@ return {
 			"go",
 			"haskell",
 		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-space>",
-				node_incremental = "<C-space>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
-			},
-		},
+		-- incremental_selection = {
+		-- 	enable = true,
+		-- 	keymaps = {
+		-- 		init_selection = "<C-space>",
+		-- 		node_incremental = "<C-space>",
+		-- 		scope_incremental = false,
+		-- 		node_decremental = "<bs>",
+		-- 	},
+		-- },
 	},
-	config = function(_, opts)
-		if type(opts.ensure_installed) == "table" then
-			---@type table<string, boolean>
-			local added = {}
-			opts.ensure_installed = vim.tbl_filter(function(lang)
-				if added[lang] then
-					return false
-				end
-				added[lang] = true
-				return true
-			end, opts.ensure_installed)
-		end
-		require("nvim-treesitter.configs").setup(opts)
-	end,
 }
